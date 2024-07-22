3c8207b55cf393e35de7aa388b18c14993109b33
Vincenzo Marrazzo <72307974+xzan8189@users.noreply.github.com>
2024-07-22 23:18:47 +0200

    third commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  output: 'export',
+  basePath: '/reaction',
+  assetPrefix: '/reaction/',
+  images: {
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
