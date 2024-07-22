afeb847272461d189d68f3988166e8d22c0bb8dc
Vincenzo Marrazzo <72307974+xzan8189@users.noreply.github.com>
2024-07-22 21:56:42 +0200

    Updates

---
+++ _next/static/chunks/app/page-8b7c3827ce7ec7b5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{9385:function(e,n,s){Promise.resolve().then(s.bind(s,4759)),Promise.resolve().then(s.bind(s,5488))}},function(e){e.O(0,[230,453,176,971,23,744],function(){return e(e.s=9385)}),_N_E=e.O()}]);
