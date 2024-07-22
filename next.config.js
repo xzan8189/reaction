dd41157f419bfeeea02f61710efe1ae525c077a9
Vincenzo Marrazzo <72307974+xzan8189@users.noreply.github.com>
2024-07-23 00:22:46 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/reaction' : '',
+  assetPrefix: isProd ? '/reaction/' : '',
+  images: {
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
